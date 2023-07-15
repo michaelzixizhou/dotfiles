d31d792b8494f42c085c3a0e2cb2da73df509d9a
michaelzixizhou <106829824+michaelzixizhou@users.noreply.github.com>
2023-07-15 03:12:30 -0400

    Fixed LSP signature bug with noice.nvim

--- nvim/lua/plugins/_ui.lua
+++ nvim/lua/plugins/_ui.lua
@@ -54,6 +54,9 @@ return {
                     ["vim.lsp.util.stylize_markdown"] = true,
                     ["cmp.entry.get_documentation"] = true,
                 },
+                signature = {
+                    enabled = false
+                }
             },
             presets = {
                 bottom_search = true,
