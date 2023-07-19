f4329e05616641c8ac6a70d4681d94563dc1c9cf
michaelzixizhou <106829824+michaelzixizhou@users.noreply.github.com>
2023-07-18 23:42:53 -0400

    Whichkey configs

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -2,5 +2,3 @@ require("config.keymaps")
 require("config.options")
 require("config.autocmds")
 
-local defaults = {
-}
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -41,7 +41,7 @@ return {
     },
     {
         "folke/which-key.nvim",
-        config = true
+        event = "VeryLazy",
     },
     -- Better `vim.notify()`
     {
